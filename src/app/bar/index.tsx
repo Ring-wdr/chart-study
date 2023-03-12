318d6ef165c2a4d11c826e61b1977d85be3ea1d4
Ring-Wdr <akswnd55@gmail.com>
2023-03-12 14:16:06 +0900

    simple bar graph with dynamic mock data

---
+++ src/app/bar/index.tsx
@@ -0,0 +1,63 @@
+import { CSSProperties } from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  ChartData,
+  ChartOptions,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+import { faker } from "@faker-js/faker";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options: ChartOptions<"bar"> = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top",
+    },
+    title: {
+      display: true,
+      text: "Chart.js Bar Chart",
+    },
+  },
+};
+
+const labels = ["January", "February", "March", "April", "May", "June", "July"];
+
+export const data: ChartData<"bar"> = {
+  labels,
+  datasets: [
+    {
+      label: "Dataset 1",
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      backgroundColor: "rgba(255, 99, 132, 0.5)",
+    },
+    {
+      label: "Dataset 2",
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      backgroundColor: "rgba(53, 162, 235, 0.5)",
+    },
+  ],
+};
+
+export const style: CSSProperties = {
+  width: "40rem",
+  height: "30rem",
+};
+
+export const BarPage = () => {
+  return <Bar options={options} data={data} style={style} />;
+};
