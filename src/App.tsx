3ea48ccb8f8ffd5de5415e769f72d4309a282245
Ring-Wdr <akswnd55@gmail.com>
2023-03-12 14:15:19 +0900

    mock data + router

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,28 @@
-import { useEffect, useState } from "react";
-import {
-  Chart,
-  LineElement,
-  PointElement,
-  CategoryScale,
-  LinearScale,
-  Tooltip,
-  Legend,
-  ChartData,
-} from "chart.js";
-import { Line } from "react-chartjs-2";
+import { Suspense } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Layout } from "./component/layout";
+import { routeArr } from "./router";
 import "./App.css";
 
-Chart.register(
-  LineElement,
-  PointElement,
-  CategoryScale,
-  LinearScale,
-  Tooltip,
-  Legend
-);
-
 function App() {
-  const [data, setData] = useState<string[][]>([]);
-  const chartData: ChartData<"line"> = {
-    labels: ["2021", "2022", "2023"],
-    datasets: [
-      {
-        label: "년도",
-        data: [3, 2, 3],
-        borderColor: "aqua",
-        tension: 0.4,
-      },
-    ],
-  };
-  useEffect(() => {
-    (async () => {
-      const dd = await fetch(
-        "/ml/machine-learning-databases/iris/iris.data"
-      ).then((res) => res.text());
-      setData(dd.split("\n").map((row: string) => row.split(",")));
-    })();
-  }, []);
-
   return (
-    <div className="App">
-      <Line data={chartData} />
-    </div>
+    <Router>
+      <Layout>
+        <Routes>
+          {routeArr.map(([key, path, SingleRoute]) => (
+            <Route
+              key={key}
+              path={path}
+              element={
+                <Suspense fallback={<div>Loading</div>}>
+                  <SingleRoute />
+                </Suspense>
+              }
+            />
+          ))}
+        </Routes>
+      </Layout>
+    </Router>
   );
 }
 
